287e30885429a55b1d4d992bb99b316565295b57
Ferdinant2000 <firdavskomolitdinov@gmail.com>
2025-09-02 18:58:03 +0500

    Начало работы с добавлением App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Resume from './components/Resume';
-import Works from './components/Works';
-import Blogs from './components/Blogs';
-import Contact from './components/Contact';
+// src/App.tsx
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Works from "./components/Works";
+import Blogs from "./components/Blogs";
+import Contact from "./components/Contact";
 
-const App: React.FC = () => {
-    return (
-        <div>
-            <Navbar />
-            <About />
-            <Resume />
-            <Works />
-            <Blogs />
-            <Contact />
-        </div>
-    );
-};
+export default function App() {
+  const [activeSection, setActiveSection] = useState("about");
 
-export default App;
+  const renderSection = () => {
+    switch (activeSection) {
+      case "about":
+        return <About />;
+      case "resume":
+        return <Resume />;
+      case "works":
+        return <Works />;
+      case "blogs":
+        return <Blogs />;
+      case "contact":
+        return <Contact />;
+      default:
+        return <About />;
+    }
+  };
+
+  return (
+    <div className="flex h-screen bg-gray-900 text-white">
+      {/* Navbar слева */}
+      <Navbar setActiveSection={setActiveSection} activeSection={activeSection} />
+      
+      {/* Основной контент */}
+      <main className="flex-1 p-6 overflow-y-auto">
+        {renderSection()}
+      </main>
+    </div>
+  );
+}
